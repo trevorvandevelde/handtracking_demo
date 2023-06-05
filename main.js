769d3a52e14646a525258274686bb2bfc514efb4
Trevor Van de Velde <vantrevor@berkeley.edu>
2023-06-04 21:04:07 -0700

    Updates

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-//import { GUI } from 'dat.gui';
-//import { Hands } from '@mediapipe/hands';
-//import { Hands } from 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0'
 
 const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-//const canvasCtx = canvasElement.getContext('2d');
+
+var red_light = document.getElementById("red_light");
+var green_light = document.getElementById("green_light");
+red_light.opacity = .1;
+green_light.opacity = .1;
+
 
 let detections = {};
 
+//if you wanted to use three.js objects
 const cubeLeft = new THREE.Mesh( new THREE.SphereGeometry(.3, 8, 8) , new THREE.MeshBasicMaterial( {color: 0x00ff00} )); 
 cubeLeft.position.set(.5, .8, 0);
 cubeLeft.visible = false;
 
-
-
-
 const cubeRight = new THREE.Mesh( new THREE.SphereGeometry(.3, 8, 8) , new THREE.MeshBasicMaterial( {color: 0xff0000} )); 
 cubeRight.position.set(-.5, .8, 0);
 cubeRight.visible = false;
-
-const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
-pointLight.position.set(0, 0, 0 );
-
-pointLight.visible = false;
-    
-const sphereSize = 2;
-const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
 
 
 
@@ -36,32 +27,31 @@ function onResults(results) {
   detections = results;
   cubeLeft.visible = false;
   cubeRight.visible = false;
+  red_light.style.opacity = .1;
+  green_light.style.opacity = .1;
 
-  pointLight.visible = false;
   if (results.multiHandedness){
     for (const handednessLandmarks of results.multiHandedness){
-        //console.log(handednessLandmarks.label);
+
         if (handednessLandmarks.label == 'Right' ){
-
-            cubeLeft.visible = true;
-            pointLight.visible = true;
-            //if right hand add red light bulb
+            //cubeLeft.visible = true;
+            green_light.style.opacity = 1; //show green light.
         } else {
-
-            cubeRight.visible = true;
-            //cubeLeft.visible = false;
-            //if left hand add green light bulb
+            //cubeRight.visible = true;
+            red_light.style.opacity = 1; // show red light.
         }
     }
   }
 }
+
+//mediapipes hand tracking
 
 const hands = new Hands({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
 }});
 
 hands.setOptions({
-  maxNumHands: 2,
+  maxNumHands: 4, //set for 4 hands max
   modelComplexity: 1,
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5
@@ -82,22 +72,14 @@ cameraHand.start();
 /////////////////////////Video Stream
 
 var videoTexture,videoSettings, rawVideoStream, videoStream, audioTrack;
-//let gui = new GUI();
-
-
 var renderer, scene, camera;
 
 navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function(stream){
     rawVideoStream = stream;
     videoSettings=stream.getVideoTracks()[0].getSettings();
-    console.log("videoSettings: width=%d, height=%d, frameRate=%d", videoSettings.width, videoSettings.height, videoSettings.frameRate);
-    //audioTrack = stream.getAudioTracks()[0];
-    
-    //making a sperate pure video stream is a workaround
-    //let videoStream = new MediaStream(stream.getVideoTracks());
 
     videoTexture = new THREE.VideoTexture(videoElement);
-    //videoTexture.minFilter = THREE.LinearFilter;
+
     init();
 
 }).catch(function(error){console.error(error);});
@@ -107,19 +89,12 @@ function init() {
 	let w = videoSettings.width;
 	let h = videoSettings.height;
 
-	//Renderer setup
-	document.body.style = "overflow: hidden;";
-	var container = document.createElement("div");
-  
-	document.body.appendChild(container);
+    //renderer setup
 	renderer = new THREE.WebGLRenderer({antialias: false});
 	renderer.setSize(w, h);
-   
+
+    var container = document.getElementsByClassName("container")[0];
 	container.appendChild(renderer.domElement);
-
-    var instructions = document.createTextNode("Raise Right Hand for Red. Raise Left Hand for Green");
-    document.body.appendChild(instructions);
-
 
 	//Scene setup:
 	scene = new THREE.Scene();
@@ -134,25 +109,14 @@ function init() {
 	camera = new THREE.OrthographicCamera(-1,1,1,-1);
 	camera.position.z = 10;
 	scene.add(camera);
-
-    //const camera = new THREE.PerspectiveCamera(
-       // 75,
-       // window.innerWidth / window.innerHeight,
-       // 0.1,
-       // 1000
-    //)
-    //camera.position.z = 7
     
-    scene.add( cubeLeft );
-    scene.add( cubeRight);
 
+    //scene.add( cubeLeft );
+    //scene.add( cubeRight );
 
-	
 	videoStream = renderer.domElement.captureStream(videoSettings.frameRate);
-    console.log("something should have happened");
 
     setInterval(function() {
-        
 
         renderer.render(scene, camera);
     
